0b350d05a98ff11790b3435386ee5f62077feb40
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-10-13 14:51:18 +0200

    Update proxy-chain version

--- lib/proxy_chain_rb/server.rb
+++ lib/proxy_chain_rb/server.rb
@@ -1,16 +1,18 @@
 module ProxyChainRb
   class Server
-    attr_accessor :script_path, :instance_id, :command, :pid, :generated_proxy_url
+    attr_accessor :script_path, :instance_id, :pid_command, :pid, :generated_proxy_url
   
     def initialize(instance_id = nil)
       self.script_path          =   File.expand_path("../node_js/proxy-chain-server/server.js", __FILE__)
       self.instance_id          =   instance_id.to_s.empty? ? SecureRandom.hex : instance_id
-      self.command              =   "ps aux | awk '/proxy-chain-instance-id-#{instance_id}/'"
+      self.pid_command          =   "ps aux | grep \"[n]ode\" | awk '/proxy-chain-instance-id-#{self.instance_id}/'"
       self.generated_proxy_url  =   nil
     end
-  
-    def start(proxy_url, wait: 3)
-      IO.popen("((node #{self.script_path} \"#{proxy_url}\" proxy-chain-instance-id-#{instance_id} &)&)") do |io|
+    
+    def start(proxy_url, tag: "default", started_at: Time.now, wait: 3)
+      command                   =   "((node #{self.script_path} \"#{proxy_url}\" proxy-chain-instance-id-#{self.instance_id} tag-#{tag} started-#{started_at.to_i} &)&)"
+      
+      IO.popen(command) do |io|
         self.generated_proxy_url   =   io.gets&.strip
       end
       
@@ -23,7 +25,7 @@ module ProxyChainRb
     end
   
     def identify_pid
-      process                   =   `#{self.command}`.split("\n")&.select { |p| p =~ /proxy-chain-instance-id-#{self.instance_id}$/i }&.first
+      process                   =   `#{self.pid_command}`.split("\n")&.select { |p| p =~ /proxy-chain-instance-id-#{self.instance_id}/i }&.first
       parts                     =   process&.split(' ')
       pid                       =   parts && parts.any? ? parts[1] : nil
       self.pid                  =   !pid.to_s.empty? ? pid.to_i : nil
